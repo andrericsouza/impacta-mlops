e169078e678d219eeb5f52169e48f931833342dd
andrericsouza <andrericsouza@yahoo.com.br>
2026-06-11 21:55:46 -0300

    aula 03

--- src/data.py
+++ src/data.py
@@ -3,9 +3,11 @@ import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 
+
 def load_diamonds():
 
     return sns.load_dataset("diamonds")
+
 
 def split_features_target(df: pd.DataFrame):
 
@@ -13,6 +15,7 @@ def split_features_target(df: pd.DataFrame):
     X = df.drop(columns=["price"])
 
     return X, y
+
 
 def train_test_split_diamonds(test_size: float = 0.2, random_state: int = 42):
 
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,11 +1,20 @@
-from src.data import train_test_split_diamonds
+import pandas as pd
+
+from src.data import load_diamonds, train_test_split_diamonds
+
+
+def test_load_diamonds():
+
+    df = load_diamonds()
+
+    assert isinstance(df, pd.DataFrame)
+
+    assert not df.empty
 
 
 def test_train_test_split():
 
-    X_train, X_test, y_train, y_test = (
-        train_test_split_diamonds()
-    )
+    X_train, X_test, y_train, y_test = train_test_split_diamonds()
 
     assert len(X_train) > 0
     assert len(X_test) > 0
